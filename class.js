50730901c184c1f7e82af6c3a6ae0f53d3016526
jinkyu.kim <jinkyukim.dev@gmail.com>
2019-12-01 21:08:16 +0900

    Update files

---
+++ class.js
@@ -0,0 +1,30 @@
+class Person {
+     constructor (name, first, second) {
+         this.name = name;
+         this.first = first;
+         this.second = second;
+    }
+    sum() {
+        return "prototype's result: " + (this.first + this.second);
+    }
+}
+
+
+
+
+var kim = new Person('jinkyu', 10, 20);
+var park = new Person('juyoung',50,50)
+
+kim.sum = function () {
+    return "this : " + (this.first + this.second);
+}
+
+console.log('kim', kim);
+console.log("kim.sum()", kim.sum());
+console.log('park', park)
+console.log("park.sum()", park.sum());
+
+
+
+
+
